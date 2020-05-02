cf2402c12aada761f7bef40e40a88cd0248ef977
Stian Grenborgen <stian.grenborgen@nav.no>
2020-05-02 21:50:43 +0200

    Oppdaterer etter endring i felles-log.

--- legacy/src/main/java/no/nav/vedtak/prosesstask/legacy/TaskAuditlogger.java
+++ legacy/src/main/java/no/nav/vedtak/prosesstask/legacy/TaskAuditlogger.java
@@ -11,6 +11,7 @@ import no.nav.vedtak.log.audit.Auditdata;
 import no.nav.vedtak.log.audit.AuditdataHeader;
 import no.nav.vedtak.log.audit.Auditlogger;
 import no.nav.vedtak.log.audit.CefField;
+import no.nav.vedtak.log.audit.CefFieldName;
 import no.nav.vedtak.log.audit.CefFields;
 import no.nav.vedtak.log.audit.EventClassId;
 
@@ -50,18 +51,16 @@ public class TaskAuditlogger {
     
     private Set<CefField> createFields(ProsessTaskData data) {
         final Set<CefField> fields = new HashSet<>();
-        fields.add(new CefField(CefFields.EVENT_TIME, System.currentTimeMillis()));
-        fields.add(new CefField(CefFields.REQUEST, data.getTaskType()));
+        fields.add(new CefField(CefFieldName.EVENT_TIME, System.currentTimeMillis()));
+        fields.add(new CefField(CefFieldName.REQUEST, data.getTaskType()));
         if (data.getAktørId() != null) {
-            fields.add(new CefField(CefFields.BERORT_BRUKER_ID, data.getAktørId()));
+            fields.add(new CefField(CefFieldName.BERORT_BRUKER_ID, data.getAktørId()));
         }
         if (data.getFagsakId() != null) {
-            fields.add(new CefField(CefFields.SAKSNUMMER_VERDI, data.getFagsakId()));
-            fields.add(new CefField(CefFields.SAKSNUMMER_LABEL, CefFields.SAKSNUMMER_TEXT));
+            fields.addAll(CefFields.forSaksnummer(data.getFagsakId()));
         }
         if (data.getBehandlingId() != null) {
-            fields.add(new CefField(CefFields.BEHANDLING_VERDI, data.getBehandlingId()));
-            fields.add(new CefField(CefFields.BEHANDLING_LABEL, CefFields.BEHANDLING_TEXT));   
+            fields.addAll(CefFields.forBehandling(data.getBehandlingId()));
         }
         return fields;
     }
