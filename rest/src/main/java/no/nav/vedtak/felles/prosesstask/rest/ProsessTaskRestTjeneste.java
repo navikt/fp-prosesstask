7b541d2f88ccc6c2d99f102543e9a3e7664c906a
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-21 17:46:07 +0200

    fix: mediatype retur verdi fra ProsessTaskRestTjeneste (#53)

--- rest/src/main/java/no/nav/vedtak/felles/prosesstask/rest/ProsessTaskRestTjeneste.java
+++ rest/src/main/java/no/nav/vedtak/felles/prosesstask/rest/ProsessTaskRestTjeneste.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -46,6 +47,7 @@ import no.nav.vedtak.sikkerhet.abac.TilpassetAbacAttributt;
 
 @OpenAPIDefinition(tags = @Tag(name = "prosesstask", description = "Håndtering av asynkrone oppgaver i form av prosesstask"))
 @Path("/prosesstask")
+@Produces(MediaType.APPLICATION_JSON)
 @RequestScoped
 @Transactional
 public class ProsessTaskRestTjeneste {
@@ -68,7 +70,7 @@ public class ProsessTaskRestTjeneste {
     @Path("/create")
     @Consumes(MediaType.APPLICATION_JSON)
     @Operation(description = "Oppretter en prosess task i henhold til request", summary = "Oppretter en ny task klar for kjøring.", tags = "prosesstask", responses = {
-            @ApiResponse(responseCode = "202", description = "Prosesstaskens oppdatert informasjon"),
+            @ApiResponse(responseCode = "202", description = "Prosesstaskens oppdatert informasjon", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProsessTaskDataDto.class))),
             @ApiResponse(responseCode = "500", description = "Feilet pga ukjent feil eller tekniske/funksjonelle feil")
     })
     @BeskyttetRessurs(action = CREATE, property = ABAC_DRIFT_ATTRIBUTT)
