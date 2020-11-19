11ce19d39960c263afd07386c81bc3169b1d555d
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-11-19 11:45:16 +0100

    fix: migrering satt opp annerledes her for maven - kjører alltid

--- task/src/test/java/no/nav/vedtak/felles/prosesstask/JpaExtension.java
+++ task/src/test/java/no/nav/vedtak/felles/prosesstask/JpaExtension.java
@@ -2,25 +2,14 @@ package no.nav.vedtak.felles.prosesstask;
 
 import javax.persistence.EntityManager;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import no.nav.vedtak.felles.jpa.TransactionHandler;
 import no.nav.vedtak.felles.jpa.TransactionHandler.Work;
 import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareExtension;
-import no.nav.vedtak.util.env.Environment;
 
 public class JpaExtension extends EntityManagerAwareExtension {
 
-    private static final Logger LOG = LoggerFactory.getLogger(JpaExtension.class);
-    private static final boolean isNotRunningUnderMaven = Environment.current().getProperty("maven.cmd.line.args") == null;
-
     static {
-        if (isNotRunningUnderMaven) {
-            LOG.info("Kjører IKKE under maven");
-            // prøver alltid migrering hvis endring, ellers funker det dårlig i IDE.
-            Databaseskjemainitialisering.migrerUnittestSkjemaer();
-        }
+        Databaseskjemainitialisering.migrerUnittestSkjemaer();
         Databaseskjemainitialisering.settJdniOppslag();
     }
 
@@ -42,6 +31,5 @@ public class JpaExtension extends EntityManagerAwareExtension {
     public <R> R doInTransaction(Work<R> func) throws Exception {
         return doInTransaction(getEntityManager(), func);
     }
-
 
 }
