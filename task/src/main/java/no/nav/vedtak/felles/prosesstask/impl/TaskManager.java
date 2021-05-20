4ce01fb59ec7f2e734a8440bab5d574acc4c1855
Jan Olav Eide <80393418+jan-olaveide@users.noreply.github.com>
2021-05-20 11:37:14 +0200

    log warn siden dette kan være kritisk (#167)
    Co-authored-by: Jan-Olav Eide <jan-olav.eide@nav.no>

--- task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/TaskManager.java
+++ task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/TaskManager.java
@@ -75,8 +75,8 @@ public class TaskManager implements AppServiceHandler {
     private int maxNumberOfTasksToPoll = getSystemPropertyWithLowerBoundry(TASK_MANAGER_POLLING_TASKS_SIZE, numberOfTaskRunnerThreads, 0);
 
     /**
-     * Ventetid før neste polling forsøk (antar dersom task ikke plukkes raskt nok, kan en annen poller ta over).
-     * (sekunder)
+     * Ventetid før neste polling forsøk (antar dersom task ikke plukkes raskt nok,
+     * kan en annen poller ta over). (sekunder)
      */
     private long waitTimeBeforeNextPollingAttemptSecs = getSystemPropertyWithLowerBoundry(TASK_MANAGER_POLLING_WAIT, 30L, 1L);
 
@@ -121,7 +121,10 @@ public class TaskManager implements AppServiceHandler {
 
         if (dispatcher != null) {
 
-            /** Holder styr på kjørende task per tråd slik at vi kan i enkelttilfeller hente ut info om det på en tråd. */
+            /**
+             * Holder styr på kjørende task per tråd slik at vi kan i enkelttilfeller hente
+             * ut info om det på en tråd.
+             */
             class TrackCurrentDispatchedTask implements ProsessTaskDispatcher {
                 ProsessTaskDispatcher delegate = selectProsessTaskDispatcher(dispatcher);
 
@@ -158,9 +161,10 @@ public class TaskManager implements AppServiceHandler {
             if (dispatcherList.size() == 1) {
                 return dispatcherList.get(0);
             } else {
-                // kast exception har fler enn 2 instanser tilgjengelig, vet ikke hvilken vi skal velge
+                // kast exception har fler enn 2 instanser tilgjengelig, vet ikke hvilken vi
+                // skal velge
                 throw new IllegalArgumentException(
-                    "Utvikler-feil: har flere mulige instanser å velge mellom, vet ikke hvilken som skal benyttes: " + dispatcherList);
+                        "Utvikler-feil: har flere mulige instanser å velge mellom, vet ikke hvilken som skal benyttes: " + dispatcherList);
             }
         } else {
             throw new IllegalArgumentException("Utvikler-feil: skal ikke komme hit (unsatifisied dependency) - har ingen ProsessTaskDispatcher");
@@ -199,8 +203,9 @@ public class TaskManager implements AppServiceHandler {
             startTaskThreads();
             startPollerThread();
         } else {
-            log.info("Kan ikke starte {}, ingen tråder konfigurert, sjekk om du har konfigurert kontaineren din riktig bør ha minst en cpu tilgjengelig.",
-                getClass().getSimpleName());
+            log.info(
+                    "Kan ikke starte {}, ingen tråder konfigurert, sjekk om du har konfigurert kontaineren din riktig bør ha minst en cpu tilgjengelig.",
+                    getClass().getSimpleName());
         }
     }
 
@@ -226,11 +231,12 @@ public class TaskManager implements AppServiceHandler {
         }
         if (pollingService == null) {
             this.pollingService = Executors
-                .newSingleThreadScheduledExecutor(new NamedThreadFactory(threadPoolNamePrefix + "-poller", false)); //$NON-NLS-1$
+                    .newSingleThreadScheduledExecutor(new NamedThreadFactory(threadPoolNamePrefix + "-poller", false)); //$NON-NLS-1$
         }
         this.pollingServiceScheduledFutures = List.of(
-            pollingService.scheduleWithFixedDelay(new PollAvailableTasks(), delayBetweenPollingMillis / 2, delayBetweenPollingMillis, TimeUnit.MILLISECONDS),
-            pollingService.scheduleWithFixedDelay(new FreeBlockedTasks(), 750L, 7500L, TimeUnit.MILLISECONDS));
+                pollingService.scheduleWithFixedDelay(new PollAvailableTasks(), delayBetweenPollingMillis / 2, delayBetweenPollingMillis,
+                        TimeUnit.MILLISECONDS),
+                pollingService.scheduleWithFixedDelay(new FreeBlockedTasks(), 750L, 7500L, TimeUnit.MILLISECONDS));
 
         // schedulerer første runde, reschedulerer seg selv senere.
         pollingService.schedule(new MoveToDonePartition(), 30, TimeUnit.SECONDS);
@@ -247,7 +253,8 @@ public class TaskManager implements AppServiceHandler {
     }
 
     /**
-     * Poller for tasks og logger jevnlig om det ikke er ledig kapasitet (i in-memory queue) eller ingen tasks funnet (i db).
+     * Poller for tasks og logger jevnlig om det ikke er ledig kapasitet (i
+     * in-memory queue) eller ingen tasks funnet (i db).
      */
     protected synchronized List<IdentRunnable> pollForAvailableTasks() {
 
@@ -260,7 +267,7 @@ public class TaskManager implements AppServiceHandler {
 
         int numberOfTasksToPoll = Math.min(capacity, maxNumberOfTasksToPoll);
         TaskManagerGenerateRunnableTasks pollDatabaseToRunnable = new TaskManagerGenerateRunnableTasks(getTaskDispatcher(), this::pollTasksFunksjon,
-            this::submitTask);
+                this::submitTask);
         List<IdentRunnable> tasksFound = pollDatabaseToRunnable.execute(numberOfTasksToPoll);
 
         reportRegularlyIfNoTasksFound(now, tasksFound);
@@ -284,9 +291,9 @@ public class TaskManager implements AppServiceHandler {
         if (capacity < 1) {
             long round = pollerRoundNoCapacityRounds.incrementAndGet();
             if (round % 60 == 0) {
-                log.info(
-                    "Ingen ledig kapasitet i siste polling runder siden [{}].  Sjekk eventuelt om tasks blir kjørt eller om de henger/er treghet under kjøring.",
-                    pollerRoundNoCapacitySince.get());
+                log.warn(
+                        "Ingen ledig kapasitet i siste polling runder siden [{}].  Sjekk eventuelt om tasks blir kjørt eller om de henger/er treghet under kjøring.",
+                        pollerRoundNoCapacitySince.get());
             }
             // internal work queue already full, no point trying to push more
             return true;
@@ -303,7 +310,7 @@ public class TaskManager implements AppServiceHandler {
         Set<Long> inmemoryTaskIds = getRunTaskService().getTaskIds();
 
         List<ProsessTaskEntitet> tasksEntiteter = taskManagerRepository
-            .pollNesteScrollingUpdate(numberOfTasksStillToGo, waitTimeBeforeNextPollingAttemptSecs, inmemoryTaskIds);
+                .pollNesteScrollingUpdate(numberOfTasksStillToGo, waitTimeBeforeNextPollingAttemptSecs, inmemoryTaskIds);
 
         return tasksEntiteter.stream().map(readTaskFunksjon).collect(Collectors.toList());
     }
@@ -336,7 +343,8 @@ public class TaskManager implements AppServiceHandler {
         }
 
         /**
-         * simple backoff interval in seconds per round to account for transient database errors.
+         * simple backoff interval in seconds per round to account for transient
+         * database errors.
          */
         private final int[] backoffInterval = new int[] { 1, 2, 5, 5, 10, 10, 10, 10, 30 };
         private final AtomicInteger backoffRound = new AtomicInteger();
@@ -367,7 +375,7 @@ public class TaskManager implements AppServiceHandler {
                 Thread.currentThread().interrupt();
             } catch (JDBCConnectionException e) { // NOSONAR
                 backoffRound.incrementAndGet();
-                log.warn( "PT-739415 Transient datase connection feil, venter til neste runde (runde={}): {}: {}",
+                log.warn("PT-739415 Transient datase connection feil, venter til neste runde (runde={}): {}: {}",
                         backoffRound.get(), e.getClass(), e.getMessage());
             } catch (Exception e) { // NOSONAR
                 backoffRound.set(backoffInterval.length - 1); // force max delay (skal kun havne her for Exception/RuntimeException)
@@ -391,7 +399,7 @@ public class TaskManager implements AppServiceHandler {
             } catch (Throwable fatal) {
                 /**
                  * skal aldri komme hit, men fange og håndtere exception før
-                 * 
+                 *
                  * @see ScheduledExecutorService#scheduleWithFixedDelay
                  */
                 log.error("Polling fatal feil, logger exception men tråden vil bli drept", fatal);
@@ -502,7 +510,10 @@ public class TaskManager implements AppServiceHandler {
 
     }
 
-    /** Unblokkerer tasks som har blitt konservativt blokkert (der den som kjører ikke ser nye veto påga Read Committed tx isolation level. */
+    /**
+     * Unblokkerer tasks som har blitt konservativt blokkert (der den som kjører
+     * ikke ser nye veto påga Read Committed tx isolation level.
+     */
     class FreeBlockedTasks implements Runnable {
 
         /** splittet fra for å kjøre i {@link TransactionHandler}. */
@@ -542,15 +553,17 @@ public class TaskManager implements AppServiceHandler {
 
     }
 
-    /** Internal executor that also tracks ids of currently queue or running tasks. */
+    /**
+     * Internal executor that also tracks ids of currently queue or running tasks.
+     */
     class IdentExecutorService {
 
         private final ThreadPoolExecutor executor;
 
         IdentExecutorService() {
             executor = new ThreadPoolExecutor(numberOfTaskRunnerThreads, numberOfTaskRunnerThreads, 0L, TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<>(maxNumberOfTasksToPoll),
-                new NamedThreadFactory(threadPoolNamePrefix + "-runtask", true)) { //$NON-NLS-1$
+                    new ArrayBlockingQueue<>(maxNumberOfTasksToPoll),
+                    new NamedThreadFactory(threadPoolNamePrefix + "-runtask", true)) { //$NON-NLS-1$
 
                 @Override
                 protected void afterExecute(Runnable r, Throwable t) {
@@ -583,9 +596,9 @@ public class TaskManager implements AppServiceHandler {
 
         Set<Long> getTaskIds() {
             return executor.getQueue().stream()
-                .map(IdentFutureTask.class::cast)
-                .map(IdentFutureTask::getId)
-                .collect(Collectors.toSet());
+                    .map(IdentFutureTask.class::cast)
+                    .map(IdentFutureTask::getId)
+                    .collect(Collectors.toSet());
         }
 
         void stop() {
