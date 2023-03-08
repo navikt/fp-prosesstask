d30c56312fac47e1bdab66588a984cd9a7408890
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-03-08 09:11:53 +0100

    Dropp auditlogg ved stat, siste feles

--- kontekst/src/main/java/no/nav/vedtak/prosesstask/kontekst/KontekstCdiProsessTaskDispatcher.java
+++ kontekst/src/main/java/no/nav/vedtak/prosesstask/kontekst/KontekstCdiProsessTaskDispatcher.java
@@ -3,7 +3,6 @@ package no.nav.vedtak.prosesstask.kontekst;
 import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
 import no.nav.vedtak.exception.IntegrasjonException;
 import no.nav.vedtak.exception.TekniskException;
@@ -12,7 +11,6 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
 import no.nav.vedtak.felles.prosesstask.api.TaskType;
 import no.nav.vedtak.felles.prosesstask.impl.BasicCdiProsessTaskDispatcher;
 import no.nav.vedtak.felles.prosesstask.impl.ProsessTaskHandlerRef;
-import no.nav.vedtak.felles.prosesstask.log.TaskAuditlogger;
 import no.nav.vedtak.log.mdc.MDCOperations;
 import no.nav.vedtak.log.mdc.MdcExtendedLogContext;
 import no.nav.vedtak.sikkerhet.kontekst.BasisKontekst;
@@ -25,12 +23,8 @@ import no.nav.vedtak.sikkerhet.kontekst.KontekstHolder;
 public class KontekstCdiProsessTaskDispatcher extends BasicCdiProsessTaskDispatcher {
     private static final MdcExtendedLogContext LOG_CONTEXT = MdcExtendedLogContext.getContext("prosess"); //$NON-NLS-1$
 
-    private final TaskAuditlogger taskAuditlogger;
-
-    @Inject
-    public KontekstCdiProsessTaskDispatcher(TaskAuditlogger taskAuditlogger) {
+    public KontekstCdiProsessTaskDispatcher() {
         super(Set.of(TekniskException.class, IntegrasjonException.class));
-        this.taskAuditlogger = taskAuditlogger;
     }
 
     @Override
@@ -49,7 +43,6 @@ public class KontekstCdiProsessTaskDispatcher extends BasicCdiProsessTaskDispatc
 
             taskHandler.doTask(task);
 
-            taskAuditlogger.logg(task);
             // renser ikke LOG_CONTEXT her. tar alt i RunTask slik at vi kan logge exceptions også
         }
     }
