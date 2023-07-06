5cb5b251802235d8420a572883e035eecd392aad
Michal J. Sladek <michal.sladek@nav.no>
2023-07-06 15:55:41 +0200

    Fix sonar smells.

--- task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/ProsessTaskRepository.java
+++ task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/ProsessTaskRepository.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.hibernate.jpa.HibernateHints;
 import org.hibernate.query.NativeQuery;
