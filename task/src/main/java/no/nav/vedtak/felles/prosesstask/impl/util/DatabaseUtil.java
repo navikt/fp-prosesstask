8a75909bdd9162147914b7a52af40218fa32919c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-12-08 20:23:33 +0100

    Fjerne behov for hibernate.dialect

--- task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/util/DatabaseUtil.java
+++ task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/util/DatabaseUtil.java
@@ -1,32 +1,62 @@
 package no.nav.vedtak.felles.prosesstask.impl.util;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+
 import jakarta.persistence.EntityManager;
 
 public class DatabaseUtil {
+
+    private static final Map<String, String> CACHE_DIALECT = new ConcurrentHashMap<>();
 
     private DatabaseUtil() {
     }
 
     public static boolean isPostgres(EntityManager entityManager) {
-        return getDialect(entityManager).contains("PostgreSQL");
+        return getDatabaseDialect(entityManager).filter(DatabaseUtil::isPostgres).isPresent();
     }
 
     public static boolean isOracle(EntityManager entityManager) {
-        return getDialect(entityManager).contains("Oracle");
+        return getDatabaseDialect(entityManager).filter(DatabaseUtil::isOracle).isPresent();
     }
 
     public static String getDialect(EntityManager entityManager) {
-        return (String) entityManager.getEntityManagerFactory().getProperties().get("hibernate.dialect");
+        return getDatabaseDialect(entityManager).orElse("unknown");
     }
 
     public static String getSqlForUniktGruppeNavn(EntityManager entityManager) {
-        if (DatabaseUtil.isPostgres(entityManager)) {
+        var dialect = getDatabaseDialect(entityManager);
+        if (dialect.filter(DatabaseUtil::isPostgres).isPresent()) {
             return "SELECT nextval('seq_prosess_task_gruppe')";
         }
-        if (DatabaseUtil.isOracle(entityManager)) {
+        if (dialect.filter(DatabaseUtil::isOracle).isPresent()) {
             return "SELECT seq_prosess_task_gruppe.nextval FROM dual";
         }
-        throw new UnsupportedOperationException("Unsupported Database: " + DatabaseUtil.getDialect(entityManager));
+        throw new UnsupportedOperationException("Unsupported Database: " + getDatabaseDialect(entityManager).orElse("unknown"));
+    }
 
+    private static Optional<String> getDatabaseDialect(EntityManager entityManager) {
+        if (entityManager.getEntityManagerFactory() instanceof SessionFactoryImplementor sfi) {
+            var key = sfi.getUuid();
+            if (!CACHE_DIALECT.containsKey(key)) {
+                var dialectClass = sfi.getJdbcServices().getDialect();
+                if (dialectClass != null) {
+                    CACHE_DIALECT.put(key, dialectClass.getClass().getSimpleName().toLowerCase());
+                }
+            }
+            return Optional.ofNullable(CACHE_DIALECT.get(key));
+        }
+        return Optional.empty();
+    }
+
+    private static boolean isOracle(String s) {
+        return s.startsWith("oracle");
+    }
+
+    private static boolean isPostgres(String s) {
+        return s.startsWith("postgres");
     }
 }
