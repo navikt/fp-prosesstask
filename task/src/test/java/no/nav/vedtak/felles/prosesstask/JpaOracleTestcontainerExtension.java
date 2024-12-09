443fe4adf52a003b7bf152c06df10fb157451045
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-12-09 12:34:23 +0100

    feat: bruker PathParam for task status og id for enklere input i swagger-ui. (#744)
    * feat: bruker PathParam for task status og id for enklere input i swagger-ui.
    
    * refactor: bruker exhaustiv switch for mapping.

--- task/src/test/java/no/nav/vedtak/felles/prosesstask/JpaOracleTestcontainerExtension.java
+++ task/src/test/java/no/nav/vedtak/felles/prosesstask/JpaOracleTestcontainerExtension.java
@@ -1,12 +1,13 @@
 package no.nav.vedtak.felles.prosesstask;
 
-import no.nav.foreldrepenger.konfig.Environment;
-
 import org.testcontainers.oracle.OracleContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import no.nav.foreldrepenger.konfig.Environment;
+
 public class JpaOracleTestcontainerExtension extends JpaExtension {
-    private static final String TEST_DB_CONTAINER = Environment.current().getProperty("testcontainer.oracle.test.db", String.class, "gvenzl/oracle-free:23-slim-faststart");
+    private static final String TEST_DB_CONTAINER = Environment.current()
+        .getProperty("testcontainer.oracle.test.db", String.class, "gvenzl/oracle-free:23-slim-faststart");
     private static final OracleContainer TEST_DATABASE;
 
     static {
