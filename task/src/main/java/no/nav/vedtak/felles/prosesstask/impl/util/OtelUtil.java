a4fa69f8223a0e22e055464e5bb9142766a4306d
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-12-10 09:28:45 +0100

    feat: basis opentelemetry støtte i for prosesstask (#746)
    * feat: basis opentelemetry støtte i for prosesstask.
    
    * Review
    
    * Rydder versjonering

---
+++ task/src/main/java/no/nav/vedtak/felles/prosesstask/impl/util/OtelUtil.java
@@ -0,0 +1,39 @@
+package no.nav.vedtak.felles.prosesstask.impl.util;
+
+import java.util.function.UnaryOperator;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.SpanKind;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.log.tracing.OtelSpanWrapper;
+
+public class OtelUtil {
+
+    private static OtelSpanWrapper WRAPPER = new OtelSpanWrapper(GlobalOpenTelemetry.getTracer("fp-prosesstask"));
+
+    public static OtelSpanWrapper wrapper() {
+        return WRAPPER;
+    }
+
+    private OtelUtil() {
+        // Sonar
+    }
+
+    public static UnaryOperator<SpanBuilder> taskAttributter(ProsessTaskData data) {
+        return spanBuilder -> {
+            var builder = spanBuilder
+                .setAttribute("prosesstaskId", data.getId())
+                .setAttribute("prosesstaskType", data.taskType().value());
+            if (data.getSaksnummer() != null) {
+                builder = builder.setAttribute("saksnummer", data.getSaksnummer());
+            }
+            if (data.getBehandlingUuid() != null) {
+                builder = builder.setAttribute("behandlingUuid", data.getBehandlingUuid().toString());
+            } else if (data.getBehandlingId() != null) {
+                builder = builder.setAttribute("behandlingId", data.getBehandlingId());
+            }
+            return builder.setSpanKind(SpanKind.INTERNAL);
+        };
+    }
+}
