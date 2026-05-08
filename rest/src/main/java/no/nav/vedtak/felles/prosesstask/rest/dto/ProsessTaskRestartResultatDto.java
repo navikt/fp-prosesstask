f240ce4e6c1a34d4382be232f72545cd4ffb79d8
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-05-08 15:22:59 +0200

    Chore: moderniser jackson feilobjekt og annoteringer (#1019)

--- rest/src/main/java/no/nav/vedtak/felles/prosesstask/rest/dto/ProsessTaskRestartResultatDto.java
+++ rest/src/main/java/no/nav/vedtak/felles/prosesstask/rest/dto/ProsessTaskRestartResultatDto.java
@@ -2,19 +2,16 @@ package no.nav.vedtak.felles.prosesstask.rest.dto;
 
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 @Schema(description = "Resultatet av asynkron-restart av en eksisterende prosesstask")
 public class ProsessTaskRestartResultatDto {
--- rest/src/main/java/no/nav/vedtak/felles/prosesstask/rest/dto/ProsessTaskRetryAllResultatDto.java
+++ rest/src/main/java/no/nav/vedtak/felles/prosesstask/rest/dto/ProsessTaskRetryAllResultatDto.java
@@ -3,21 +3,18 @@ package no.nav.vedtak.felles.prosesstask.rest.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 @Schema(description = "Resultatet av asynkron-restart av feilede prosesstasks")
 public class ProsessTaskRetryAllResultatDto {
